59e48654a4e2c7ace6815d588a68a6ae556c1fe7
turkirr <turkird@msn.com>
2019-08-31 17:41:24 +0300

    day9

---
+++ Day9.js
@@ -0,0 +1,17 @@
+var val1;
+var val2;
+var result;
+
+val1 = 3;
+val2 = 2;
+result = val1 * val2 ;
+document.write("The result of the Multiply of two numbers " + result);
+
+document.write("</br>")
+
+result = val1 % val2;
+document.write("The rest of the division " + result);
+
+document.write("</br>")
+
+document.write("The Power of to values " + Math.pow(val1,val2));
